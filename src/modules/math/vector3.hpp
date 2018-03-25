20513a5784cf53bf33d559d0b2c830eba7d79005
Rafael Campos Nunes <rafaelnunes@engineer.com>
2018-03-25 19:05:09 -0300

    Add a modules directory and moves some stuff
    This isn't a final job because I'm not entirely sure of the architecture
    of this huge thing.
    
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/modules/math/vector3.hpp
+++ src/modules/math/vector3.hpp
@@ -1,4 +1,3 @@
-
 /**
  * MIT License
  *
@@ -28,6 +27,7 @@
 #define VECTOR_H
 
 #include <cmath>
+
 // TODO: Limit the instantiation types to numbers only.
 template <class T>
 class Vector3 {
@@ -41,19 +41,19 @@ public:
 	m_y = y;
 	m_z = z;
     }
-    
+
     ~Vector3() {}
-    
+
     Vector3<T> norm() {
 	Vector3<T> normalized = *this;
-	
+
 	normalized.m_x /= mag();
 	normalized.m_y /= mag();
 	normalized.m_z /= mag();
-	
-	return normalized;
+
+        return normalized;
     }
-    
+
     T mag() {
 	return  sqrt(pow(m_x, 2) + pow(m_y, 2) + pow(m_z, 2));
     }
